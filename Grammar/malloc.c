6e81da701afac0d1d3da5d75a2a3fbad31201a29
WildfootTW <wild.foot.yee.tzwu@gmail.com>
2017-02-01 21:37:22 +0800

    add grammer

---
+++ Grammar/malloc.c
@@ -0,0 +1,22 @@
+//#include <stdlib.h>
+
+//    void *malloc(size_t size);
+//      allocates [$size] bytes
+//      returns a pointer to the allocated memory
+
+//    void free(void *ptr);
+//      frees the memory space pointed to by [$*ptr]
+//      if free(ptr) has already been called before 
+//          undefined behavior occurs
+//      if [$ptr] is NULL
+//          no operation is performed
+
+//    void *calloc(size_t nmemb, size_t size);
+//      allocates memory for an array of [$nmemb] elements of [$size] bytes each 
+//      returns a pointer to the allocated memory
+
+//    void *realloc(void *ptr, size_t size);
+//      changes the size of the memory block pointed to by [$ptr] to [$size] bytes
+//      the contents will be unchanged in the range from the start of the region up to the minimum of the old and new sizes
+
+
