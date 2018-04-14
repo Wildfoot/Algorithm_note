cd499d6196a47ff361abd8a3a41bf96b092955e9
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-04-15 00:54:49 +0800

    yo

--- Data_Structure/Standard Template Library/priority_queue.cpp
+++ Data_Structure/Standard Template Library/priority_queue.cpp
@@ -20,7 +20,7 @@ class cp{
         return a>b;//小於:由小而大 ，大於:由大而小 ，但取出(top())時是從最後面取出 
     }
 };
-//或用struct 
+//或用struct
 /*struct cp{
     bool operator()(int &a, int &b){
         return a<b;
@@ -29,19 +29,16 @@ class cp{
 
 int main(int argc, char** argv) {
     priority_queue<int> pq;
-    //priority_queue<int,vector<int>,cp> pq;//自訂順序 
-    //priority_queue<int,vector<int>,greater<int> > pq;//由大到小 
-    pq.push(555);
-    pq.push(222);
-    pq.push(333);
-    pq.push(22);
-    pq.push(2252);
-    pq.push(333);
-    
+    //priority_queue<int,vector<int>,cp> pq;//自訂順序
+    //priority_queue<int,vector<int>,greater<int> > pq;//由大到小
+
+    for(int n:{555, 222, 333, 22, 2252, 333})
+        pq.push(n);
+
     print_queue(pq);
-    cout<<"pq.top="<<pq.top()<<endl;
-    cout<<"length="<<pq.size()<<endl;
-    
+    cout << " pq.top = " << pq.top() << endl;
+    cout << " length = " << pq.size() << endl;
+
     while(!pq.empty()){
         cout<<pq.top()<<",";
         pq.pop();
